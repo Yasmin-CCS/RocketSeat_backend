c3d39be465288e4db0e3576ce02143294689d4bb
Yasmin <yas.carozzi@gmail.com>
2023-07-04 08:43:01 -0300

    finalização backend

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 
 const TagsController = require("../controllers/TagsController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
 
 const tagsRoutes = Router();
 
@@ -8,6 +10,6 @@ const tagsRoutes = Router();
 const tagsController = new TagsController();
 
 
-tagsRoutes.get("/:user_id", tagsController.index);
+tagsRoutes.get("/", ensureAuthenticated, tagsController.index);
 
 module.exports = tagsRoutes;
